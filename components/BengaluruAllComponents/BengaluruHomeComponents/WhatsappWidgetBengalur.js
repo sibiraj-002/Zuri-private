d5779c8986827cc07affcfe00b9cbadf2cb0ac76
JPivista <jpivistasolutions@gmail.com>
2024-04-18 19:21:38 +0530

    new whatsapp code is updated

--- components/BengaluruAllComponents/BengaluruHomeComponents/WhatsappWidgetBengalur.js
+++ components/BengaluruAllComponents/BengaluruHomeComponents/WhatsappWidgetBengalur.js
@@ -1,49 +1,30 @@
 'use client'
-import { useEffect } from 'react';
+import React from 'react'
+import { FaWhatsapp } from "react-icons/fa6";
+import Link from 'next/link'
+
+import { Col } from 'react-bootstrap'
 
 const ChatWidget = () => {
-    useEffect(() => {
-        const url = 'https://wati-integration-prod-service.clare.ai/v2/watiWidget.js?34483';
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.async = true;
-        script.src = url;
-        script.onload = () => {
-            const options = {
-                "enabled": true,
-                "chatButtonSetting": {
-                    "backgroundColor": "#00e785",
-                    "ctaText": "Chat with us",
-                    "borderRadius": "25",
-                    "marginLeft": "0",
-                    "marginRight": "20",
-                    "marginBottom": "20",
-                    "ctaIconWATI": true,
-                    "position": "right"
-                },
-                "brandSetting": {
-                    "brandName": "The Zuri Hotels",
-                    "brandSubTitle": "The Zuri Whitefield, Bengaluru <br> Mon - Sat (10am - 6pm)",
-                    "brandImg": "https://www.wati.io/wp-content/uploads/2023/04/Wati-logo.svg",
-                    "welcomeText": "Hi there!\nHow can I help you?",
-                    "messageText": "Hello, %0A I have an enquiry.",
-                    "backgroundColor": "#800080",
-                    "ctaText": "Chat with us",
-                    "borderRadius": "25",
-                    "autoShow": false,
-                    "phoneNumber": "9739820008"
-                }
-            };
-            window.CreateWhatsappChatWidget(options);
-        };
-        document.body.appendChild(script);
 
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
+    const phone = '+919739820008'
+    const text = 'Hello%20I%20have%20an%20enquiry.'
 
-    return null; // Since this component only adds a script and doesn't render anything
+    return (
+        <Col className='position-fixed end-0 bottom-0 p-lg-5 p-3'>
+            <Link href={`https://wa.me/${phone}?text=${text}`} target='_blank' className='text-decoration-none' >
+
+                <button type="button" class="btn btn-success d-flex flex-row  justify-content-center align-items-center rounded-4" >
+                    {/* <div className='whatsapp-hover-show'>
+                        Chat with us
+                    </div> */}
+                    <div>
+                        <FaWhatsapp size="45" />
+                    </div>
+                </button>
+            </Link>
+        </Col>
+    )// Since this component only adds a script and doesn't render anything
 };
 
 export default ChatWidget;
