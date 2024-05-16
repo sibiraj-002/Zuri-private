b9e24ec15b67fb8e6032e504a4158b358f9b8d5b
JPivista <jpivistasolutions@gmail.com>
2024-05-16 17:38:22 +0530

    mobile synxis updarted

--- components/Shared/Header/MobileHeader.js
+++ components/Shared/Header/MobileHeader.js
@@ -1,13 +1,16 @@
 'use client'
 import Link from 'next/link'
-import React, { useState } from 'react'
-import { Col, Container, Image, Row } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Button, Col, Container, Image, Row } from 'react-bootstrap'
+import { useRouter } from 'next/navigation';
 
 import '../../../app/globals.css'
 
 const MobileHeader = () => {
 
     const [isFormShow, setIsFormShow] = useState(false);
+    const router = useRouter();
+
 
     const [isShow, setIsShow] = useState(false);
     const [activeSubMenu, setActiveSubMenu] = useState(null);
@@ -34,30 +37,68 @@ const MobileHeader = () => {
     };
 
     const hotels = [
-        { id: 1, name: 'The Zuri Kumarakom, Kerala Resort & Spa' },
-        { id: 2, name: 'The Zuri White Sands, Goa Resort & Casino' },
-        { id: 3, name: 'The Zuri Whitefield, Bengaluru' },
+        { id: 1, name: 'The Zuri Kumarakom, Kerala Resort & Spa', value: '64786' },
+        { id: 2, name: 'The Zuri White Sands, Goa Resort & Casino', value: '64787' },
+        { id: 3, name: 'The Zuri Whitefield, Bengaluru', value: '27305' },
     ];
 
     const HotelReservationForm = () => {
+        // const defaultSelectedHotel = '64786'; // Set the default selected hotel value
         const [selectedHotel, setSelectedHotel] = useState('');
         const [selectedCheckInDate, setSelectedCheckInDate] = useState(getTodayDateString());
         const [selectedCheckOutDate, setSelectedCheckOutDate] = useState(getTodayDateString());
         const [selectedRooms, setSelectedRooms] = useState(1);
         const [selectedAdults, setSelectedAdults] = useState(1);
         const [selectedChildren, setSelectedChildren] = useState(0);
+        const [selectedChain, setSelectedChain] = useState('');
+
+        useEffect(() => {
+            // Reset the form when the pathname changes
+            setSelectedHotel('');
+            setSelectedCheckInDate(getTodayDateString());
+            setSelectedCheckOutDate(getTodayDateString());
+            setSelectedRooms(1);
+            setSelectedAdults(1);
+            setSelectedChildren(0);
+            setSelectedChain('');
+            setHotelValidationError(false);
+        }, [router.pathname]);
+
+        const [hotelValidationError, setHotelValidationError] = useState(false); // Track validation error
 
         const handleFormSubmit = (e) => {
             e.preventDefault();
-            // Handle form submission logic here, e.g., send data to backend
-            console.log('Form submitted:', {
-                selectedHotel,
-                selectedCheckInDate,
-                selectedCheckOutDate,
-                selectedRooms,
-                selectedAdults,
-                selectedChildren,
-            });
+
+            if (!selectedHotel) {
+                setHotelValidationError(true);
+                return;
+            }
+            if (selectedHotel === '64786') {
+                setSelectedChain('17869')
+            }
+            if (selectedHotel === '64787') {
+                setSelectedChain('17869')
+            }
+            if (selectedHotel === '27305') {
+                setSelectedChain('10237')
+            }
+
+            // Construct the URL based on form data
+            const reservationURL = `https://be.synxis.com/?adult=${selectedAdults}&arrive=${selectedCheckInDate}&chain=${selectedChain}&child=${selectedChildren}&currency=INR&depart=${selectedCheckOutDate}&hotel=${selectedHotel}&level=hotel&locale=en-US&rooms=${selectedRooms}`;
+            // console.log(reservationURL)
+            // Navigate to the reservation URL
+            router.push(reservationURL);
+            setHotelValidationError(false);
+
+            // Reset form data
+            setSelectedHotel('');
+            setSelectedCheckInDate(getTodayDateString());
+            setSelectedCheckOutDate(getTodayDateString());
+            setSelectedRooms(1);
+            setSelectedAdults(1);
+            setSelectedChildren(0);
+            setSelectedChain('');
+            setHotelValidationError(false);
         };
 
         function getTodayDateString() {
@@ -269,16 +310,27 @@ const MobileHeader = () => {
                                         <div>
                                             <select
                                                 id="hotel"
+                                                className='bg-white'
                                                 value={selectedHotel}
                                                 onChange={(e) => setSelectedHotel(e.target.value)}
+                                                required
+                                                style={{ borderColor: hotelValidationError ? 'red' : '' }}
+
                                             >
                                                 <option value="">Select a hotel</option>
                                                 {hotels.map((hotel) => (
-                                                    <option key={hotel.id} value={hotel.name}>
-                                                        {hotel.name}
+                                                    <option key={hotel.id} value={hotel.value}>
+                                                        <div className='bg-white'>
+                                                            <p className='p-4'>
+                                                                {hotel.name}
+                                                            </p>
+                                                        </div>
                                                     </option>
                                                 ))}
                                             </select>
+                                            {!selectedHotel && hotelValidationError && (
+                                                <p style={{ color: 'red', fontSize: '12px' }}>Please select a hotel</p>
+                                            )}
                                         </div>
                                     </div>
 
@@ -397,13 +449,12 @@ const MobileHeader = () => {
 
                                     <div className='d-flex flex-column gap-4'>
                                         <div className='text-center d-flex flex-column gap-2'>
-                                            <Link
-                                                href="https://be.synxis.com/?adult=1&arrive=2023-12-20&chain=17869&child=0&currency=INR&depart=2023-12-21&hotel=64787&level=hotel&locale=en-US&rooms=1"
+                                            <Button onClick={handleFormSubmit}
                                                 className='text-decoration-none text-white px-2 py-2 d-inline-block'
                                                 style={{ background: 'purple' }}
                                             >
                                                 BOOK NOW
-                                            </Link>
+                                            </Button>
 
                                             <Link
                                                 href="https://be.synxis.com/?_ga=2.164956023.936920120.1702967799-2038087003.1664255723&adult=1&arrive=2023-12-20&chain=17869&child=0&currency=INR&depart=2023-12-21&hotel=64786&level=hotel&locale=en-US&rooms=1&shell=GCF&start=searchres&template=GCF"
