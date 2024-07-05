9c448a77302f7844f36eee57ee300a1652d237e4
sibiraj-002 <shansibi2002@gmail.com>
2024-07-05 16:32:24 +0530

    Zuri hotel gallery updated

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
 const config = {
-    apiUrl: 'https://docs.thezurihotels.com/',
-    wpApiUrl: 'https://docs.thezurihotels.com/wp-json/wp/v2',
-    wpPageApi: 'https://docs.thezurihotels.com/wp-json/wp/v2/pages'
+    apiUrl: 'https://ads.thezurihotels.com/',
+    wpApiUrl: 'https://ads.thezurihotels.com/wp-json/wp/v2',
+    wpPageApi: 'https://ads.thezurihotels.com/wp-json/wp/v2/pages'
 
 };
 
 export default config;
+
+
+// https://ads.thezurihotels.com/
+
+// Old Docs
+// apiUrl: 'https://docs.thezurihotels.com/',
+// wpApiUrl: 'https://docs.thezurihotels.com/wp-json/wp/v2',
+// wpPageApi: 'https://docs.thezurihotels.com/wp-json/wp/v2/pages'
