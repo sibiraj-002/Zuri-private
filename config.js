3acdea0a3f2faf36249e6574f87a3436e07d558c
sibiraj-002 <shansibi2002@gmail.com>
2024-07-15 14:39:23 +0530

    everything is up-to-date

--- config.js
+++ config.js
@@ -1,12 +1,10 @@
 const config = {
-    apiUrl: 'https://ads.thezurihotels.com/',
-    wpApiUrl: 'https://ads.thezurihotels.com/wp-json/wp/v2',
-    wpPageApi: 'https://ads.thezurihotels.com/wp-json/wp/v2/pages'
-
+  apiUrl: "https://docs.thezurihotels.com/",
+  wpApiUrl: "https://docs.thezurihotels.com/wp-json/wp/v2",
+  wpPageApi: "https://docs.thezurihotels.com/wp-json/wp/v2/pages",
 };
 
 export default config;
-
 
 // https://ads.thezurihotels.com/
 
@@ -14,3 +12,13 @@ export default config;
 // apiUrl: 'https://docs.thezurihotels.com/',
 // wpApiUrl: 'https://docs.thezurihotels.com/wp-json/wp/v2',
 // wpPageApi: 'https://docs.thezurihotels.com/wp-json/wp/v2/pages'
+
+//-------------------------------------------------------------------------------------------------------------------------------------------//
+
+// New Ads
+// const config = {
+//     apiUrl: 'https://ads.thezurihotels.com/',
+//     wpApiUrl: 'https://ads.thezurihotels.com/wp-json/wp/v2',
+//     wpPageApi: 'https://ads.thezurihotels.com/wp-json/wp/v2/pages'
+
+// };
